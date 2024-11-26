240b5e498bf111723c8aec761e79c54625ee3062
Jose Libreros <jdlm970319@gmail.com>
2024-11-25 19:23:30 -0500

    Cambios hechos en la rama DEVELOP

--- example1.py
+++ example1.py
@@ -1,3 +1,3 @@
 print("Hola estudiantes de SCV")
 print("Hola estudiantes de SCV 2")
-print("Hola estudiantes de SCV 2")
+print("Hola estudiantes de SCV 4, rama DEVELOP")
---
+++ new file.js
@@ -0,0 +1,3 @@
+function testFunctionDevelop() {
+    console.log("Hello develop branch");
+}
